eca5fcd20fae8cda885e2575fc323001b324738a
Zak Gogi <zakgogi1@gmail.com>
2021-07-24 12:09:54 +0100

    added heroku fetch

--- db/1_setup.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS scores;
-
-CREATE TABLE scores (
-    id serial PRIMARY KEY,
-    name varchar(255) NOT NULL,
-    score int NOT NULL
-);
--- db/2_seed.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO scores (name, score) 
-VALUES
-    ('Zak 1', 128 ),
-    ('Ellie', 65 ),
-    ('Zak 2', 147),
-    ('Default', 20),
-    ('Test', 6);
